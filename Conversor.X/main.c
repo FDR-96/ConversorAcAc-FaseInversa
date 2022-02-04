1e1b634ea66b4d4550fadc64405d3dc05ab88a2b
Federico <roldanfede96@gmail.com>
2022-02-03 21:37:37 -0300

    update1

--- Conversor.X/main.c
+++ Conversor.X/main.c
@@ -8,8 +8,8 @@
 #include <util/delay.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-     
+#include <stdlib.h>     
+#include <math.h>
 
 //Librerias propias.
 #include "Librerias/INT/INT.h"
@@ -22,10 +22,10 @@
 const uint16_t tMax = 163; //Tiempo maximo que puede alcanzar el timer sin desbordar 
 const uint16_t rComp = 256; //Registro de comparacion maximo de 8bit
 const uint16_t tSemiCiclo = 100; //Tiempo de un semiciclo
-volatile uint16_t tensionRed = 110;
+volatile uint8_t tensionRed = 110;
 volatile uint16_t captura = 0; //Capturara el valor del timer
 volatile uint16_t ancho = 0; //Valor final del ancho de pulso
-volatile uint32_t angulo = 0;
+volatile uint16_t angulo = 0;
 
 volatile uint16_t msegundos = 0;
 volatile uint8_t segundos = 0;
@@ -46,16 +46,24 @@ void AjustaTensionDeRedRemoto();
 void AjustarTensionDimmerRemoto();
 //Funciones generales utilizadas por ambos modos.
 void MostrarNumero(uint16_t numero, uint8_t digitos);  //Digitos = 3: xxx / Digitos = 4: xx,xx / Digitos = 5: xxx,xx
-int CalcularTension();
+uint16_t CalcularTension();
 void AjustarTension();
 
 //Rutinas de servicio de interrupción.
 ISR(INT0_vect){
+
    ancho = tSemiCiclo -((captura*tMax)/rComp); //157 equivalen a 10ms
-   MostrarNumero(CalcularTension(), 4);
-   PORTD = (1<<4);
-   _delay_ms(ancho);
-   PORTD = (0<<4);
+     MostrarNumero(ancho, 3);
+USART_SetData('s');
+
+   //   PORTD = (1<<4);
+//   _delay_ms(1000);
+//   PORTD = (0<<4);
+}
+
+ISR(INT1_vect){
+
+ //  MostrarNumero(1111, 4);
 }
 
 ISR(TIMER0_OVF_vect) {
@@ -65,11 +73,15 @@ ISR(TIMER0_OVF_vect) {
         segundos++;
         if(segundos == 60){
             segundos = 0;
-        }else if(segundos == 5){
+        }else if(segundos == 30){
             TCCR1B|=(1<<CS12)|(0<<CS11)|(1<<CS10);
         }
     }
 }
+
+//Si el TC (de 8 bits) se ejecuta a 16 MHz, se desbordará después de 16,384 ms, lo que supera con creces el intervalo de muestra recomendado de 10 ms.
+//No es necesario comprobar si hay desbordamiento. Si borra el contador en el flanco ascendente, entonces el conteo en el flanco descendente es el 
+//ancho del pulso en microsegundos:
 
 ISR(TIMER1_CAPT_vect) {
   if(flanco) // Detectamos un flanco ascendente
@@ -95,14 +107,15 @@ void main() {
         TIMER0_init();
         TIMER1_init();
         USART_init();
-        DDRD = (0<<4);
+        DDRD = (1<<4);
         PORTD = (0<<4);
     sei();
-         MostrarNumero(123, 3);
-         USART_SetData('d');
-        MostrarNumero(2232, 4);
-                 USART_SetData('x');
-        MostrarNumero(33332, 5);
+    TIMER0_Start();
+//         MostrarNumero(123, 3);
+//         USART_SetData('d');
+//        MostrarNumero(2232, 4);
+//                 USART_SetData('x');
+//        MostrarNumero(33332, 5);
     while(1){
 
     }
@@ -111,28 +124,33 @@ void main() {
 
 void MostrarNumero(uint16_t numero, uint8_t digitos)
 {
-	uint8_t _unidades = numero%10;
-	uint8_t _decenas = (numero/10)%10;
-    uint8_t _centenas = (numero/100)%10;
-    uint8_t _milesu = (numero/1000)%10;
-    uint8_t _milesd = (numero/1000)%10;
+	uint16_t _unidades = numero%10;
+	uint16_t _decenas = (numero/10)%10;
+    uint16_t _centenas = (numero/100)%10;
+    uint16_t _milesu = (numero/1000)%10;
+    uint16_t _milesd = (numero/10000)%10;
     if(digitos == 3){
       char _tx3[3] ={_centenas + '0',_decenas + '0', _unidades + '0'};
       USART_SetArrayData(_tx3, sizeof _tx3);
     }else if(digitos == 4){
-        char _tx4[5] ={_centenas + '0',_decenas + '0',',', _unidades + '0', _milesu + '0' };
+        char _tx4[5] ={ _milesu + '0', _centenas + '0',',',_decenas + '0', _unidades + '0' };
         USART_SetArrayData(_tx4, sizeof _tx4);
-    }else{
-        char _tx5[6] ={_centenas + '0',_decenas + '0', _unidades + '0',',', _milesu + '0', _milesd + '0'};
+    }else if(digitos == 5){
+        char _tx5[6] ={_milesd + '0', _milesu + '0', _centenas + '0',',',_decenas + '0', _unidades };
         USART_SetArrayData(_tx5, sizeof _tx5);
+    }else{
+        char _tx2[6] ={_centenas + '0',_decenas + '0',',', _unidades + '0','m','s'};
+        USART_SetArrayData(_tx2, sizeof _tx2);
     }
 
 }
 
-int CalcularTension(){
-    angulo = (180*ancho)/10;
-    uint32_t _tensionCarga =  (tensionRed*sqrt(2)/PI)*(1+cos(angulo * PI/180))*10;
+uint16_t  CalcularTension(){
+    angulo = (180*ancho)/100;
+    uint16_t _tensionCarga =  (tensionRed*sqrt(2)/PI)*(1+cos(angulo * PI/180))*100;
     return _tensionCarga;
 }
 
-void AjustarTension(){}
+void AjustarTension(){
+
+}
