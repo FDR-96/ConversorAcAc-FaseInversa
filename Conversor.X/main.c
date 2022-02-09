ba85e59284d16dc1ad078a3b573c97ea95a4f38f
Federico <roldanfede96@gmail.com>
2022-02-09 17:39:53 -0300

    comentarios update2

--- Conversor.X/main.c
+++ Conversor.X/main.c
@@ -36,7 +36,7 @@ volatile uint16_t ancho = 0; //Valor final del ancho de pulso
 volatile uint16_t angulo = 0;
 volatile uint16_t usegundos = 0;
 volatile uint16_t porciento = 0;
-volatile uint8_t msegundos = 0;
+volatile uint8_t us = 0;
 volatile uint8_t flag = 0;
 volatile uint8_t contador = 0;
 
@@ -72,35 +72,48 @@ uint16_t CalcularRetardo(uint16_t tension);
 
 //Rutinas de servicio de interrupción.
 ISR(INT0_vect){
+  //Si es flanco ascendente lo dejamos pasar
   if(flanco){
-      if((msegundos >= 50 || flag > 0) && contador < 3){
+      //En el flanco de subido establecemos todos los parametros en 0, si mantenemos hasta superar los 50us
+      //o lograr un valor de flag mayor a 0 y el contador de clicks cortos es menor a 3 (por lo tanto nos encontramos
+      //en el menu local) le damos el valor de true a clickLargo. Caso contraio aumenta el contador para pasar de menu.
+      if((us >= 50 || flag > 0) && contador < 3){
         clickLargo = true;
       }else{
         contador++;
       }
   }
+  //Establecemos valores iniciales de variables que se utilizaran en la rutina del timer
   flag = 0;
-  msegundos = 0;
+  us = 0;
+  //En la primera interrupcion la variable flanco es falso por lo tanto la ponemos en true
+  //para que en la proxima interrupcion al detectar el flanco descendente entre el el if
   flanco = !flanco;
 }
 
 ISR(INT1_vect){
+    //Dimmer empieza como TRUE y su valor se maneja desde el USART
     if(dimmer){
+       //Ponemos en alto la salida PD4 durante los usegundos hallados con la logica programada
        PORTD = (1<<4);
        _delay_us(usegundos);
+       //Apagamos PD4
        PORTD = (0<<4);
     }
 }
 
 
 ISR(TIMER0_OVF_vect) {
-    msegundos++;
-    if(msegundos == 100){
-        msegundos = 0;
+    //Cada vez que se ejecute esta interrupcion (establecida para que sea en un lapso de 0,01ms)
+    //aumentamos el valor de us en 1, cuando llega a 100 tendremos 1ms, incrementamos la bandera
+    //para luego utilizarla en la logica de click largo y click corto. Ademas llamamos a la funcion
+    //reajustar tension cada medio milisegundo. y volvemos a poner el registro del timer en el valor inicial
+    us++;
+    if(us == 100){
+        us = 0;
         flag++;
         ReajustarTension(); 
-    }else if( msegundos == 50 ){
-          //  TCCR1B|=(1<<CS12)|(0<<CS11)|(1<<CS10);
+    }else if( us == 50 ){
         ReajustarTension(); 
     }
     TCNT0 = 0x63;
@@ -114,23 +127,22 @@ ISR(TIMER1_CAPT_vect) {
   if(capturar) // Detectamos un flanco ascendente
 	{
 		TCCR1B &=~ (1 << ICES1); //cambiamos el sensado establecido por el de descendente
-		TCNT1 = 0x00;
+		TCNT1 = 0x00; //Reestablecemos el valor de registro del timer para que empiece desde 0.
 	}
   else // Detectamos un flanco descendente
 	{
 		captura = ICR1; // record time at which falling edge occurs
-        ancho = tSemiCiclo -((captura*tMax)/rComp);
+        ancho = tSemiCiclo -((captura*tMax)/rComp); //Calculamos el ancho del pulso para tenerlo siempre actualizado
 		TCCR1B |= (1 << ICES1); //cambiamos el sensado establecido por el de ascendente
-        TCCR1B|=(0<<CS12)|(0<<CS11)|(0<<CS10);
 	}
-     capturar = !capturar;
+     capturar = !capturar; //Calcular empieza en true, cuando detecta un flanco ascendente se pone en falso para que
+     //en la proxima interrupcion producida por un flanco descendente tome el valor del ancho del pulso en ms.
 }
 
 
 
 void main() {
     cli();
-       // PCINT_init();
         INT_init();
         TIMER0_init();
         TIMER1_init();
@@ -143,9 +155,9 @@ void main() {
     usegundos = CalcularRetardo(tensionDeseada);
     Interfaz();
     while(1){
-     
-    
+        //Si contador aumento por INT0 entre 1 y 2, se ejecuta la funcion ModoLocal.
         if(contador == 0 || contador >= 3){
+            //Volvemos contador igual a 0 por si es mayor a 2 asi poder entrar nuevamente al menu local.
             contador = 0;
             ModoRemoto();
         }else{
@@ -211,8 +223,13 @@ void Interfaz(){
 }
 
 void AjustaTensionDeRedRemoto(){
+    //Mostramos en el max la tension
     MAX7219_displayNumberyMenu(tensionRed, 1);
+    //Obtenemos con el usart los enteros almacenados atraves de un puntero de direccion.
     USART_GetIntData(&tensionRedValida); //https://es.acervolima.com/como-devolver-multiples-valores-de-una-funcion-en-c-o-c/
+    //Si la tension de red que obtuvimos es menor a 250 que es el valor maximo que soportara la electronica y es mayor a 10 que es el valor minimo.
+    //Almacenamos la tension red valida y mostramos tanto en el display como por pantalla los datos establecidos.
+    //Por el mostramos un mensaje de alerta por pantalla
     if(tensionRedValida < 250 && tensionRedValida >= 10){
         tensionRed = tensionRedValida;
         char _confirmacion[] ={"Se establecio el valor ingresado: \n\r"};
@@ -226,8 +243,13 @@ void AjustaTensionDeRedRemoto(){
 }
 
 void AjustarTensionDimmerRemoto(){
+    //Mostramos en el max la tension
     MAX7219_displayNumberyMenu(tensionDeseada, 2);
+    //Obtenemos con el usart los enteros almacenados atraves de un puntero de direccion.
     USART_GetIntData(&tensionDeseadaValida); //https://es.acervolima.com/como-devolver-multiples-valores-de-una-funcion-en-c-o-c/
+    //Si la tension de dimer que obtuvimos es menor a la tension de red que es el valor maximo que soportara la electronica y es mayor a 5 que es el valor minimo.
+    //Almacenamos la tension valida y mostramos tanto en el display como por pantalla los datos establecidos.
+    //Por el mostramos un mensaje de alerta por pantalla
     if(tensionDeseadaValida < tensionRed - 11 && tensionDeseadaValida >= 5){
         tensionDeseada = tensionDeseadaValida;
         char _confirmacion[] ={"Se establecio el valor ingresado: \n\r"};
@@ -258,34 +280,50 @@ void ModoLocal(){
 }
 
 void AjustaTensionDeRedLocal(){
+    //Mostramos por pantalla que vamos a ajustar la tension de red de manera local.
     char _info[] ={"Ajustar Tension de Red Local \n\r"};
     USART_SetArrayData(_info, sizeof _info);
+    //Mantenemos el ciclo mientras contador sigua siendo 1, lo que significa que seguimos ajustando
+    //la tension
     while(contador == 1){
+        //Leemos el ADC
         tensionRed = ADC_GetData(0)*220.0f/1024.0f;
-       // tensionDeseada = (porciento * tensionRed)/100;
+       //Mostramos en el display
         MAX7219_displayNumberyMenu(tensionRed, 1);
         
     }
+    //Volvemos a contador = 1 para no salir del menu y que nos muestre la tension seteada en el display
     contador = 1;
+    //Volvemos click largo a falso asi no entra a la funcion nuevamente
     clickLargo = false;
+    //Mostramos un mensaje que se establecio con exito
     char _confirmacion[] ={"Se establecio una Tension de red de: \n\r"};
     USART_SetArrayData(_confirmacion, sizeof _confirmacion);
+    //Imprimimos en pantalla el valor establecido
     MostrarNumero(tensionRed, 3);
 }
 
 void AjustarTensionDimmerLocal(){
+    //Mostramos por pantalla que vamos a ajustar la tension del dimmer de manera local.
     char _info[] ={"Ajustar Tension de Dimmer Local \n\r"};
     USART_SetArrayData(_info, sizeof _info);
+    //Mantenemos el ciclo mientras contador sigua siendo 2, lo que significa que seguimos ajustando
     while(contador == 2){  
+        //Leemos el ADC, el valor resultante ira de 0 a 100 que luego multiplicara a la tension de red
+        //para no pasar su valor
         porciento = (ADC_GetData(0)*100.0f)/1024.0f;
         tensionDeseada = (porciento * tensionRed)/100;
+        //Mostramos en el display
         MAX7219_displayNumberyMenu(tensionDeseada, 2);
-         //tensionReajustada = tensionDeseada*100;
     }
+    //Volvemos a contador = 2 para no salir del menu y que nos muestre la tension seteada en el display
     contador = 2;
+    //Volvemos click largo a falso asi no entra a la funcion nuevamente
     clickLargo = false;
+    //Mostramos un mensaje que se establecio con exito
     char _confirmacion[] ={"Se establecio una Tension en la carga de: \n\r"};
     USART_SetArrayData(_confirmacion, sizeof _confirmacion);
+    //Imprimimos en pantalla el valor establecido
     MostrarNumero(tensionDeseada, 3);
 }
 
@@ -314,35 +352,44 @@ void MostrarNumero(uint16_t numero, uint8_t digitos)
 }
 
 uint16_t  CalcularTensionRMS(){
+    //Regla de 3 simples para hallar el angulo con los ms obtenidos por el timer
     angulo = (180*ancho)/100;
+    //Formula para hallar la tension RMS en la carga
     uint16_t _tensionRMSCarga =  sqrt((pow(tensionRed , 2)*(angulo*PI/180))/(2*PI)-(pow(tensionRed , 2)*(sin(2*(angulo * PI/180)))/(4*PI)))*100;
-
     return _tensionRMSCarga;
 }
 
 uint16_t  CalcularTensionDC(){
+    //Regla de 3 simples para hallar el angulo con los ms obtenidos por el timer
     angulo = (180*ancho)/100;
+    //Formula para hallar la tension DC en la carga
     uint16_t _tensionDCCarga =   (tensionRed*sqrt(2)/PI)*(1-cos(angulo * PI/180))*100;
     return _tensionDCCarga;
 }
 
 
 uint16_t CalcularRetardo(uint16_t tension){
+    //Calculamos el angulo con la tension media que le mandemos
     uint16_t _angulo = acos(tension/(tensionRed*sqrt(2)/PI)-1)* 180/PI;
-    uint16_t _usegundos = (100*_angulo)/180;
-    return 10000 - _usegundos * 100 ;
+    //Con regla de 3 simples obtenemos los micro segundos
+    uint16_t _usegundos = 1000 - ((100*_angulo)/180)* 100;
+    return _usegundos ;
 }
 
 void ReajustarTension(){
-    
-  
+    //Obtenemos los valores iniciales
     uint16_t x = CalcularTensionRMS();
     uint16_t y = (tensionDeseada*100);
     uint16_t diff = (y - x);
-    
+    //Para que no oscile bruscamente y tenga un reajuste suave. Limitamos el reajuste
+    //a 3v cada medio milisegundo.
     if(diff > 500){
         diff = 300;
     }
+    //Si el valor RMS se encuentra entre los +-1,5V del valor deseado las instrucciones
+    //del if no se ejecutan.
+    //Si la diferencia es mayor, se busca saber si la tension RMS obtenida es mayor o menor
+    //Si es mayor se le resta la diferencia a la variable de ajuste, si es menor se le suma.
     if(x>=y+150 || x<=y-150){
         if(x < y){       
           tensionReajustada = tensionReajustada + (diff );
